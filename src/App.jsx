79aa45b1bbe8a417c5826474fd2caecde3972e96
brian <liar88828@gmail.com>
2023-02-26 21:04:45 +0700

    save pdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import "./App.css";
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PdfFile } from "./components/PdfFile";
+import { PDFViewer } from "@react-pdf/renderer";
+//https://react-pdf.org/advanced#on-the-fly-rendering//
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PDFDownloadLink document={<PdfFile />} fileName="FORM.pdf">
+        {({ loading }) =>
+          loading ? <button>Loading</button> : <button> Download</button>
+        }
+      </PDFDownloadLink>
+
+
+      {/* <PDFDownloadLink document={<PdfFile />} fileName="somename.pdf">
+        {({ loading }) => (loading ? "Loading document..." : "Download now!")}
+      </PDFDownloadLink> */}
+
+      <PDFViewer style={{height:'30rem'}}>
+        <PdfFile />
+      </PDFViewer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
